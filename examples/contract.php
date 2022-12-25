70b7dbb7e9e190e39412679458ea677501ace621
sergei <jmenhome@gmail.com>
2022-12-25 19:49:17 +0200

    code refactoring, address has been added (for balanceOf method)

--- examples/contract.php
+++ examples/contract.php
@@ -9,20 +9,22 @@ try {
     $fullNode = new \IEXBase\TronAPI\Provider\HttpProvider('https://api.trongrid.io');
     $solidityNode = new \IEXBase\TronAPI\Provider\HttpProvider('https://api.trongrid.io');
     $eventServer = new \IEXBase\TronAPI\Provider\HttpProvider('https://api.trongrid.io');
+    $explorer = new \IEXBase\TronAPI\Provider\HttpProvider('https://tronscan.org');
 } catch (\IEXBase\TronAPI\Exception\TronException $e) {
     echo $e->getMessage();
 }
 
+$address = readline('Address:');
 
 try {
-    $tron = new Tron($fullNode, $solidityNode, $eventServer, null, true);
+    $tron = new Tron($fullNode, $solidityNode, $eventServer, null, $explorer);
     $contract = $tron->contract('TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t');  // Tether USDT https://tronscan.org/#/token20/TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t
 
     // Data
-    echo $contract->name();
-    echo $contract->symbol();
-    echo $contract->balanceOf();
-    echo $contract->totalSupply();
+    echo 'Name: '.$contract->name()."\n";
+    echo 'Symbol: '.$contract->symbol()."\n";
+    echo sprintf('Balance of address %s: ', $address).$contract->balanceOf($address)."\n";
+    echo 'Total supply: '.$contract->totalSupply()."\n";
     //echo  $contract->transfer('to', 'amount', 'from');
 
 
