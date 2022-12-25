081eda29df812e65bef759202fa5af34a5a9abd2
sergei <jmenhome@gmail.com>
2022-12-26 00:29:34 +0200

    code refactoring

--- examples/hex.php
+++ examples/hex.php
@@ -3,8 +3,8 @@ include_once '../vendor/autoload.php';
 
 $tron = new \IEXBase\TronAPI\Tron();
 
-$tron->toHex('TT67rPNwgmpeimvHUMVzFfKsjL9GZ1wGw8');
+echo $tron->toHex(readline('Address to Hex:'))."\n";
 //result: 41BBC8C05F1B09839E72DB044A6AA57E2A5D414A10
 
-$tron->fromHex('41BBC8C05F1B09839E72DB044A6AA57E2A5D414A10');
+$tron->fromHex(readline('From Hex:'))."\n";
 //result: TT67rPNwgmpeimvHUMVzFfKsjL9GZ1wGw8
